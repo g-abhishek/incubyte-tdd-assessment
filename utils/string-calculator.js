fa182f11a671be71fd49867b4525acb990b3f298
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-10-18 11:29:50 +0530

    added support for custom delimeters

--- utils/string-calculator.js
+++ utils/string-calculator.js
@@ -1,7 +1,12 @@
 const add = (numberStr) => {
   if (!numberStr) return 0;
 
-  const delimeters = /[\n,]/;
+  let delimeters = /[\n,]/;
+  if (numberStr.startsWith("//")) {
+    const parts = numberStr.split("\n");
+    delimeters = parts[0].substring(2);
+    numberStr = parts[1];
+  }
   const parts = numberStr.split(delimeters);
   const sum = parts.reduce((acc, num) => acc + parseInt(num), 0);
 

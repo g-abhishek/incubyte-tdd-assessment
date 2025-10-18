1243bec5d1def78c93b6931d918125218e2ed9a8
abhishekgupta01 <abhishekgupta@gofynd.com>
2025-10-18 11:07:34 +0530

    added test when single number passed

--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -3,3 +3,7 @@ const { add } = require("../utils/string-calculator");
 test("should return 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return the number when only single number passed", () => {
+    expect(add("1")).toBe(1)
+})
--- utils/string-calculator.js
+++ utils/string-calculator.js
@@ -1,6 +1,7 @@
 const add = (numberStr) => {
   if (!numberStr) return 0;
-  return 0;
+  
+  return parseInt(numberStr);
 };
 
 module.exports = { add };
